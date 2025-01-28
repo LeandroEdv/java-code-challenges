d8d1d37b28c8f19fefea6345d598458ba3e8907a
leandro Santana <leandro.edv@hotmail.com>
2025-01-28 10:47:45 -0300

    feat: bubble sort

---
+++ java-code-challenges/src/main/java/algoritmos/BubbleSort.java
@@ -0,0 +1,27 @@
+package algoritmos;
+
+import java.util.Arrays;
+
+public interface BubbleSort {
+    public static void main(String[] args) {
+        int[] vetor = {3, 6, 1, 8, 4, 62, 12, 7, 5};
+        int aux;
+        boolean controle;
+
+        for (int i = 0; i < vetor.length; i++) {
+            controle = true;
+            for (int j = 0; j < vetor.length - 1; j++) {
+                if (vetor[j] > vetor[j + 1]) {
+                    aux = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = aux;
+                    controle = false;
+                }
+            }
+            if (controle) break;
+        }
+
+        System.out.println(Arrays.toString(vetor));
+
+    }
+}
